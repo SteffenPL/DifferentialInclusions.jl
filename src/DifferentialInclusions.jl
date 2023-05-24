efbacecc3349a4f8a6e07c530be03bd08054b834
Steffen Plunder <steffen.plunder@web.de>
2023-05-24 16:38:41 +0900

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.32

---
+++ src/DifferentialInclusions.jl
@@ -0,0 +1,5 @@
+module DifferentialInclusions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DifferentialInclusions
+using Test
+
+@testset "DifferentialInclusions.jl" begin
+    # Write your tests here.
+end
