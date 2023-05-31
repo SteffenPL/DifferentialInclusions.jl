329b3af22c9f052473a57dbfb040d2a72532c792
Steffen Plunder <steffen.plunder@web.de>
2023-05-31 22:06:18 +0900

    updates tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
     
 
     cons = (
-        u -> u[1], 
+        u -> u[1] - 0.2, 
         u -> 2.0 - u[2], 
         u -> abs(u[1] - u[2]) - 1.0
     )
@@ -20,11 +20,19 @@ using Test
     prob = DIProblem(ode, cons)
 
     alg = ProjectiveMethod(OSPJ(),Euler())
-    sol = solve(prob, alg, dt = 0.0001)
+    sol = solve(prob, alg, dt = 1e-5)
 
     x_end = sol[end]
-    @test isapprox(x_end[1], 0.0, atol = 1e-3)
-    @test isapprox(x_end[2], 1.0, atol = 1e-3)
+    @test isapprox(x_end[1], 0.2, atol = 1e-3)
+    @test isapprox(x_end[2], 1.2, atol = 1e-3)
+
+
+    alg = ProjectiveMethod(PGS(),Euler())
+    sol = solve(prob, alg, dt = 1e-5)
+
+    x_end = sol[end]
+    @test isapprox(x_end[1], 0.2, atol = 1e-3)
+    @test isapprox(x_end[2], 1.2, atol = 1e-3)
 
 end
 
